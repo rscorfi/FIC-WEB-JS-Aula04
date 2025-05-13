5324f4a365cdd206eb89727e5885d751cd20d30a
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-05-12 21:48:34 -0300

    Add exercício 5

---
+++ exercícios/5/index5.js
@@ -0,0 +1,5 @@
+let largura = 5
+let altura = 10
+
+console.log(`A área de um retangulo de largura ${largura} e altura ${altura} é igual a ${largura * altura}`)
+console.log(`E seu perímetro é igual à ${(largura + altura) * 2}`)
