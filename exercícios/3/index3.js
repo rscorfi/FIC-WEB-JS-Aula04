ce61d9ef16f785fed243a51d513ed9a05aea79e9
Rafael Scorfi Gatto <rscorfi@gmail.com>
2025-05-12 21:44:30 -0300

    Criados exercícios 3 e 4

---
+++ exercícios/3/index3.js
@@ -0,0 +1,4 @@
+let dolarReal = 5.25
+
+console.log('R$ 100 é equivalente a ', 100 / dolarReal)
+console.log('R$ 250 é equivalente a ', 250 / dolarReal)
---
+++ exercícios/4/index4.js
@@ -0,0 +1,3 @@
+let lado = 4
+
+console.log(`A área do quadrado de ${lado} é igual a ${lado ** 2}`)
